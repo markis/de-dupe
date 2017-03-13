b1a6b2acb078e9a9e6412ba7af3b0434476ccf46
Markis Taylor <m@rkis.cc>
2017-03-12 20:30:51 -0700

    add rollup.config.js and make sure its not ignored going forward

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+var typescript = require('rollup-plugin-typescript');
+
+module.exports = {
+  entry: './src/cli.ts',
+  dest: 'index.js',
+  format: 'cjs',
+  moduleId: 'de-dupe',
+  moduleName: 'dedupe',
+  external: [
+    'chai',
+    'fs',
+    'path',
+    'mocha',
+    'typescript'
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
+  ]
+};
